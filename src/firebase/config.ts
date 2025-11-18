2d9e773abc0b8ff0991dcbcd17eec52a293e269e
Shadow Vybez <shadowvybez001@gmail.com>
2025-11-18 14:31:51 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9240962023-a8134",
+  "appId": "1:439620863618:web:eee81efa317c8c3ba0d69b",
+  "apiKey": "AIzaSyB3Z7E2IoFWP1aNBJc7w3tAxFeMsCtfZJs",
+  "authDomain": "studio-9240962023-a8134.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "439620863618"
+};
