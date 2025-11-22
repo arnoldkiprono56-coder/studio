b57d5ae3f1115075adbf4487f49c1e02933156a9
Shadow Vybez <shadowvybez001@gmail.com>
2025-11-22 14:27:43 +0000

    GEMINI_API = AIzaSyCXTVHIyCWBioY2mq6NtfPMcJqdkjOFleU

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,11 +1,11 @@
 import {genkit} from 'genkit';
-import {openAI} from 'genkitx-openai';
+import {googleAI} from '@genkit-ai/google-genai';
 
 export const ai = genkit({
   plugins: [
-    openAI({
-        apiKey: process.env.OPENAI_API_KEY,
+    googleAI({
+      apiKey: process.env.GEMINI_API_KEY,
     }),
   ],
-  model: 'openai/gpt-4o',
+  model: 'googleai/gemini-1.5-flash-latest',
 });
